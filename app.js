b8686429cf765d8d8fc6eabd312cc3acdd8b7501
PetrozziJP <juanpablopetrozzi@gmail.com>
2015-09-29 21:18:08 -0300

    Librerias de AngularJS y Bootstrap.

--- app.js
+++ app.js
@@ -22,8 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.get('/', function(req, res){
+   res.sendfile('./public/index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,6 +56,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
